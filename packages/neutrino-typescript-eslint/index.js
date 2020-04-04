b3a7a1e0f3cd0deae8d17b253263da4624bc3830
Konstantin Alekseev <mail@kalekseev.com>
2020-04-04 17:12:59 +0300

    Return standard packages

--- packages/neutrino-typescript-eslint/index.js
+++ packages/neutrino-typescript-eslint/index.js
@@ -2,24 +2,25 @@ const merge = require("deepmerge");
 
 module.exports = (options, extendsReact = false) => (neutrino) => {
   const defaults = {
-    parser: "@typescript-eslint/parser",
     parserOptions: {
       project: "./tsconfig.json",
     },
-    plugins: ["@typescript-eslint", "standard"],
     extends: [
       "standard",
+      "plugin:import/typescript",
       "eslint:recommended",
       "plugin:@typescript-eslint/eslint-recommended",
       "plugin:@typescript-eslint/recommended",
+      "plugin:eslint-comments/recommended",
     ]
       .concat(extendsReact ? ["plugin:react/recommended"] : [])
       .concat(["prettier", "prettier/@typescript-eslint", "prettier/standard"])
       .concat(extendsReact ? ["prettier/react"] : []),
-    settings: {
-      "import/parsers": {
-        "@typescript-eslint/parser": [".ts", ".tsx"],
-      },
+    rules: {
+      "no-debugger": process.env.NODE_ENV === "production" ? "error" : 0,
+      "no-console": process.env.NODE_ENV === "production" ? "error" : 0,
+      "prefer-const": "error",
+      "eslint-comments/no-unused-disable": "error",
     },
   };
   const mergedOptions = merge(defaults, options || {});
